11ad18f370ccc25338b33c5f465f16f011ad5c72
Delta Türk <84840824+deltatr@users.noreply.github.com>
2023-04-08 02:04:28 +0300

    ses

--- ses.py
+++ ses.py
@@ -1,1 +1,12 @@
+from gtts import gTTS
+import os 
 
+text = 'Türk Hack Team, Türkiyenin önde gelen siber güvenlik sayfasıdır'  
+language = 'tr' 
+
+
+speech = gTTS(text = text, lang = language, slow = False)   
+
+speech.save("tht.mp3")  
+
+os.system("start tht.mp3")      
